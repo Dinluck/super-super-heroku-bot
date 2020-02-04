d0078af77f9ea85680d741e8873b2842ad93f7a4
Dinluck <56742640+Dinluck@users.noreply.github.com>
2020-02-04 14:27:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,11 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on(‘message’, message => {
+    if(message.author === client.user) return;
+    if(message.content.startsWith(‘delete’)) {
+        message.channel.sendMessage(‘Привет я БОТ!’);
+     }
 });
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
